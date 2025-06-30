16aceada8aa31bdaba383dafa0f5ccc0ed920c72
Ermia <Ermiaabbasi2006@gmail.com>
2025-06-30 08:35:34 +0330

    The new version that has intensity control

--- arduino.ino
+++ arduino.ino
@@ -1,16 +1,14 @@
 
 void setup() {
   Serial.begin(9600);
-  pinMode(5, OUTPUT);  //5 should be replaced by your own LED pin
+  pinMode(5, OUTPUT); #'5' should be replaced with your own pin
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char cmd = Serial.read();
-    if (cmd == '1') {
-      digitalWrite(5, HIGH);  
-    } else if (cmd == '0') {
-      digitalWrite(5, LOW);   
+    byte receivedByte = Serial.read(); 
+    int receivedNumber = (int)receivedByte;
+    int pwmValue = map(receivedNumber, 0, 100, 0, 255);
+      analogWrite(5, pwmValue);
     }
   }
-}
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import serial
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters,ConversationHandler, ContextTypes
 
 arduino = serial.Serial('SERIAL_PORT', 9600) #SERIAL_PORT example = COM12
 
-BOT_TOKEN = 'YOUR_BOT_TOKEN' #insert your bot token here
+BOT_TOKEN = 'BOT_TOKEN' #insert your bot token here
 
+ASK_INTENSITY = 0
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Send /on or /off")
-
-async def turn_on(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    arduino.write(b'1')
-    await update.message.reply_text("Turning ON")
-
-async def turn_off(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    arduino.write(b'0')
-    await update.message.reply_text("Turning OFF")
+    await update.message.reply_text("Send the intensity on the scale of 0 to 100.")
+    return ASK_INTENSITY
+async def intensity(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    intensity = update.message.text
+    if not intensity.isdigit() or not (0 <= int(intensity) <= 100):
+        await update.message.reply_text("Please enter a number between 0 and 100.")
+        return ASK_INTENSITY
+    arduino.write(bytes([int(intensity)]))
+    await update.message.reply_text(f"Changed the intensity to {intensity}.")
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    arduino.write(bytes([int('0')]))
+    await update.message.reply_text("Canceled.")
+    return ConversationHandler.END
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("on", turn_on))
-app.add_handler(CommandHandler("off", turn_off))
+conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            ASK_INTENSITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, intensity)]
+        },
+        fallbacks=[CommandHandler("cancel", cancel)]
+    )
+app.add_handler(conv_handler)
 
 app.run_polling()
